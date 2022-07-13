a48e2302636a2445832605476646ca9aff28ef31
Raghu K <raghuk.vit@gmail.com>
2022-07-13 08:49:36 +0530

    Update roboshop-kubernetes/

---
+++ eks/eks.tf
@@ -0,0 +1,18 @@
+module "vpc" {
+  source             = "github.com/r-devops/tf-module-vpc.git"
+  PROJECT_NAME       = "roboshop"
+  ENV                = "prod"
+  CREATE_INTERNET_GW = true
+  CREATE_NAT_GW      = true
+}
+
+module "eks" {
+  source             = "github.com/r-devops/tf-module-eks.git"
+  ENV                = "prod"
+  PRIVATE_SUBNET_IDS = module.vpc.PRIVATE_SUBNET_IDS
+  PUBLIC_SUBNET_IDS  = module.vpc.PUBLIC_SUBNETS_IDS
+  DESIRED_SIZE       = 2
+  MAX_SIZE           = 2
+  MIN_SIZE           = 2
+  CREATE_ALB_INGRESS = true
+}
